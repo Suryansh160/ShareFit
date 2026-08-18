81c7d5db470ff20a966c91f281b8018717570e58
Suryansh Gupta <suryanshgupta160@gmail.com>
2026-08-19 00:58:09 +0530

    TURN server added

--- server/src/config/iceServers.js
+++ server/src/config/iceServers.js
@@ -1,3 +1,17 @@
 export function getIceServers () {
-  return [{ urls: process.env.STUN_URL || 'stun:stun.l.google.com:19302' }]
+  return [
+    {
+      urls: process.env.STUN_URL || 'stun:stun.l.google.com:19302'
+    },
+    {
+      urls: [
+        'turn:global.relay.metered.ca:80',
+        'turn:global.relay.metered.ca:80?transport=tcp',
+        'turn:global.relay.metered.ca:443',
+        'turns:global.relay.metered.ca:443?transport=tcp'
+      ],
+      username: process.env.TURN_USERNAME,
+      credential: process.env.TURN_PASSWORD
+    }
+  ]
 }
